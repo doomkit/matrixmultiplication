a6ec78b8f4bde8c654a39d40d9cf377bad9ae95e
Nick <n.shkarupa@yahoo.com>
2018-10-29 11:06:51 +0100

    Initial Commit

---
+++ MatrixMultiplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MatrixMultiplication
+//
+//  Created by Nikita Shkarupa on 29/10/2018.
+//  Copyright © 2018 Nikita Shkarupa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
